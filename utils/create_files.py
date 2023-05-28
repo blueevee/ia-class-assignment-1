303f5909fecbce4e07fde2bcd5edbcf386e0ffe7
Blueevee <evelyn.ferreira@solutis.com.br>
2023-05-28 20:45:23 -0300

    fix: add verification for connected cities

--- utils/create_files.py
+++ utils/create_files.py
@@ -1,7 +1,7 @@
 import os
 import random
 
-cidades = [
+cities = [
     "rio_de_janeiro",
     "sao_paulo",
     "belo_horizonte",
@@ -25,8 +25,17 @@ cidades = [
 def generate_random_data():
     return random.randint(100, 2000), random.randint(60, 120), random.randint(1, 5)
 
-def generate_random_cities():
-    return random.sample(cidades, 2)
+def generate_random_cities(connected_cities):
+    city1 = random.choice(cities)
+    city2 = random.choice(cities)
+
+    while city1 in connected_cities and len(connected_cities[city1]) == len(cities) - 1:
+        city1 = random.choice(cities)
+
+    while city2 == city1:
+        city2 = random.choice(cities)
+
+    return city1, city2
 
 def generate_random_coordinates():
     return random.uniform(-90, 90), random.uniform(-180, 180)
@@ -37,18 +46,24 @@ if not os.path.exists(diretorio):
     os.makedirs(diretorio)
 
 for i in range(1, 101):
-    nome_arquivo = f"map{i}.pl"
-    caminho_arquivo = os.path.join(diretorio, nome_arquivo)
+    file_name = f"map{i}.pl"
+    path_file = os.path.join(diretorio, file_name)
+    connected_cities = {} 
     
-    with open(caminho_arquivo, "w") as f:
-        for _ in range(len(cidades)*2):
-            cidade1, cidade2 = generate_random_cities()
-            distancia, velocidade, periculosidade = generate_random_data()
-            f.write(f"aresta({cidade1}, {cidade2}, {distancia}, {velocidade}, {periculosidade}).\n")
+    with open(path_file, "w") as f:
+        for _ in range(len(cities) * 2):
+            initial_city, final_city = generate_random_cities(connected_cities)
+            distance, max_speed, risk_level = generate_random_data()
+            f.write(f"aresta({initial_city}, {final_city}, {distance}, {max_speed}, {risk_level}).\n")
             
+            if initial_city in connected_cities:
+                connected_cities[initial_city].add(final_city)
+            else:
+                connected_cities[initial_city] = {final_city}
+                
         f.write("\n")
-        for cidade in cidades:
+        for cidade in cities:
             latitude, longitude = generate_random_coordinates()
             f.write(f"coordenadas({cidade}, {latitude}, {longitude}).\n")
 
-    print(f"Arquivo {nome_arquivo} criado com sucesso!")
+    print(f"Arquivo {file_name} criado com sucesso! Cidades conectadas: {connected_cities}")
