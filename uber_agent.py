771f08df22bdcf60dc81dd063a85b93324de8f09
Blueevee <evelyn.ferreira@solutis.com.br>
2023-05-29 14:46:16 -0300

    feat: add gitignore and consult prolog base

--- uber_agent.py
+++ uber_agent.py
@@ -7,8 +7,9 @@ import csv
 
 def hundred_maps(alg, initial_city, final_city, path_choice):
     """
-    Realiza 100 execuções em mapas diferentes com o algoritmo passado para encontrar o melhor caminho entre 2 cidades
-    Parameters, cria um arquivo no diretório local contendo 'Memória atual (MB)', 'Pico de memória(MB)', 'Tempo gasto (segundos)', 'Comprimento do caminho', 'Lista de cidades do caminho' para cada mapa executado.
+    Realiza 100 execuções em mapas diferentes com o algoritmo passado para encontrar o melhor caminho entre 2 cidades, cria um arquivo no diretório local contendo 'Memória atual (MB)', 'Pico de memória(MB)', 'Tempo gasto (segundos)', 'Comprimento do caminho', 'Lista de cidades do caminho' para cada mapa executado.
+
+    Parameters
     ----------
     alg : String
         Nome da abordagem a ser usada para definir o melhor caminho
@@ -44,7 +45,7 @@ def hundred_maps(alg, initial_city, final_city, path_choice):
                 query = f"consult(['{posix_map_path}', '{posix_rules_path}'])"
                 result = prolog_thread.query(query)
 
-                query = f"melhor_trajeto_a_star({initial_city}, {final_city}, {path_choice}, Caminho)"
+                query = f"melhor_trajeto_{alg}({initial_city}, {final_city}, {path_choice}, Caminho)"
                 result = prolog_thread.query(query)
                 best_result = result[0]['Caminho'] if result else result
                 result_len = len(result[0]['Caminho']) if result else 0
@@ -57,6 +58,71 @@ def hundred_maps(alg, initial_city, final_city, path_choice):
         elapsed_time = end_time - start_time
 
         csv_writer.writerow([map_path.split('/')[-1], current_memory, peak_memory, elapsed_time, result_len, best_result])
-        print(f"ENCONTROU MELHOR CAMINHO DO ARQUIVO: {i}")
+        print(f"ENCONTROU O MELHOR CAMINHO PARA O MAPA: {i}")
 
     csv_file.close()
+
+def best_path(alg, city_map, initial_city, final_city, path_choice):
+    """
+    Econtra o melhor trajeto entre `initial_city` e `final_city` de acordo com a abordagem, o mapa e a preferência escolhidas.
+
+    Parameters
+    ----------
+    alg : String
+        Nome da abordagem a ser usada para definir o melhor caminho
+    map : String
+        Caminho do arquivo prolog que representa o mapa da cidade com as arestas e coordenadas
+    initial_city : String
+        Cidade de partida
+    final_city : String
+        Cidade de destino
+    path_choice : String
+        Preferência do caminho `mais seguro`, `mais rápido`
+    """
+
+    rules_path = os.path.join(os.getcwd(), 'utils/knowledge_base_rules.pl')
+    posix_rules_path = posixpath.join(*rules_path.split(os.sep))
+
+    map_path = os.path.join(os.getcwd(), city_map)
+    posix_map_path = posixpath.join(*map_path.split(os.sep))
+
+    with PrologMQI() as mqi:
+        with mqi.create_thread() as prolog_thread:
+
+            query = f"consult(['{posix_map_path}', '{posix_rules_path}'])"
+            result = prolog_thread.query(query)
+
+            query = f"melhor_trajeto_{alg}({initial_city}, {final_city}, {path_choice}, Caminho)"
+            result = prolog_thread.query(query)
+
+    return result
+
+def what_distance(city_map, initial_city, final_city):
+    """
+    Econtra qual a distância entre `initial_city` e `final_city` caso elas estejam conectadas.
+
+    Parameters
+    ----------
+    map : String
+        Caminho do arquivo prolog que representa o mapa da cidade com as arestas e coordenadas
+    initial_city : String
+        Cidade de partida
+    final_city : String
+        Cidade de destino
+    """
+
+    rules_path = os.path.join(os.getcwd(), 'utils/knowledge_base_rules.pl')
+    posix_rules_path = posixpath.join(*rules_path.split(os.sep))
+
+    map_path = os.path.join(os.getcwd(), city_map)
+    posix_map_path = posixpath.join(*map_path.split(os.sep))
+
+    with PrologMQI() as mqi:
+        with mqi.create_thread() as prolog_thread:
+
+            query = f"consult(['{posix_map_path}', '{posix_rules_path}'])"
+            result = prolog_thread.query(query)
+            query = f"distancia({initial_city}, {final_city}, Distancia)"
+            result = prolog_thread.query(query)
+
+    return result
